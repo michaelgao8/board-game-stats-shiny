38d4d249e35c6fe60dbbf82e5601ea8ba5fa9ef4
Michael <michael@Michaels-Mac-mini.local>
2021-04-19 14:42:40 -0400

    change username

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-Rscript -e "rsconnect::setAccountInfo(name='michaelgao8', token='${SHINYAPPS_TOKEN}', secret='${SHINYAPPS_SECRET}')"
+Rscript -e "rsconnect::setAccountInfo(name='michaelgao', token='${SHINYAPPS_TOKEN}', secret='${SHINYAPPS_SECRET}')"
 sh -c "Rscript $*"
