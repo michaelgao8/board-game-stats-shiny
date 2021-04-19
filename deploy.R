682eecd79f5dda39872a9f8c82ef7bce864e9196
Michael <michael@Michaels-Mac-mini.local>
2021-04-19 19:28:32 -0400

    Add bgexports to app deploy package

--- deploy.R
+++ deploy.R
@@ -17,7 +17,7 @@ setAccountInfo(name   = error_on_missing_name("SHINY_ACC_NAME"),
 
 # Deploy the application.
 deployApp(
-    appFiles = c("app.R" #, "renv/activate.R","renv.lock",'.Rprofile'
+    appFiles = c("app.R", "BGStatsExport.json" #, "renv/activate.R","renv.lock",'.Rprofile'
                        ),
     appName = error_on_missing_name("MASTERNAME"),
     appTitle = "shinyapplication")
